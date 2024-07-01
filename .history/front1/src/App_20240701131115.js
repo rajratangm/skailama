f99109270f6efc0e9fbfb7c36ce04822798846bd
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-07-01 13:11:34 +0530

    this

---
+++ .history/front1/src/App_20240701131115.js
@@ -0,0 +1,65 @@
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+// import Navbar from './Components/HomePage/Navbar/Navbar';
+// import HomePage from './Components/HomePage/HomePage/HomePage';
+// import UploadPage from './Components/UploadPage/UploadPage'; // Import UploadPage component
+// import './App.css';
+
+// const theme = createTheme({
+//   // Your theme configurations
+// });
+
+// function App() {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Router>
+//         <div className="App">
+//           <Navbar />
+//           <main className="main-content">
+//             <Routes>
+//               <Route path="/" element={<HomePage />} />
+//               <Route path="/:projectName/upload" element={<UploadPage />} /> {/* Add this route */}
+//             </Routes>
+//           </main>
+//         </div>
+//       </Router>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './';
+import HomePage from './Components/HomePage/HomePage/HomePage';
+import UploadPage from './Components/UploadPage/UploadPage';
+import SettingIndex from './Components/Setting/SettingIndex'; // Import SettingIndex component
+import './App.css';
+
+
+function App() {
+  return (
+   
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/:projectName/upload" element={<UploadPage />} />
+              <Route path="/settings" element={<SettingIndex />} /> {/* Add this route */}
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    
+  );
+}
+
+export default App;
+
